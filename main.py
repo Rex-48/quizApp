c915ebcbe62e0256b5e772d1e97548f6e195f0e7
Ether <etherus.rex48x@gmail.com>
2023-08-16 07:36:47 +0530

    Version 01 done

--- main.py
+++ main.py
@@ -1,11 +1,85 @@
 import requests
+import random
+import html
+
 
 url = "https://opentdb.com/api.php?amount=20&category=18"
 
 respose = requests.get(url)
 
-data = respose.json()
+unsorted_data = respose.json()
 
-result = data['results'] 
+Sorted_data = unsorted_data['results'] 
 
-print(result)
+
+# print(result[1])
+# print("the lenght of the dictionary is ",len(result))
+
+print(" \n ")
+
+
+
+def decode_html(encoded_text):
+    decoded_text = html.unescape(encoded_text)
+    return decoded_text
+
+
+def add_element_randomly(input_list, element):
+    random_index = random.randint(0, len(input_list))
+    input_list.insert(random_index, element)
+    return input_list
+
+
+
+def interface(category ,question_type ,question ,correct_ans ,incorrect_ans ,options ,i):
+    print("Question Type : ",question_type)
+    if question_type == "multiple":
+        print("Question ",i," : ",decode_html(question))
+        print("a. "+options[0])
+        print("b. "+options[1])
+        print("c. "+options[2])
+        print("d. "+options[3])
+        print(" ")
+
+        user = str(input(" "))
+        if user in incorrect_ans:
+            print("incorrect Answer")
+        else:
+            print("correct Answer")
+
+
+    else:
+        print("Question ",i," : ",decode_html(question))
+        print("a. True")
+        print("b. False")
+
+        print(" ")
+        user = str(input(" "))
+        if user in incorrect_ans:
+            print("incorrect Answer")
+        else:
+            print("correct Answer")
+            
+
+    print("\n")
+
+
+
+for i in range(len(Sorted_data)):
+
+    matter = Sorted_data[i]
+
+    category = matter['category']
+    question_type = matter['type']
+    question = matter['question']
+    correct_ans = matter['correct_answer']
+    incorrect_ans = matter['incorrect_answers']
+    options = add_element_randomly(incorrect_ans,correct_ans)
+    interface(category ,question_type ,question ,correct_ans ,incorrect_ans ,options ,i)
+
+    
+
+
+
+
+
